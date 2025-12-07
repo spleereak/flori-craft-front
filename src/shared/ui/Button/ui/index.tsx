bba583c4d74bccd4e1fc2d6202e7bc674ef9fb0a
spleereak <spleereak@gmail.com>
2025-12-08 00:09:25 +0300

    feat: сделал кнопку под мобилку

--- src/shared/ui/Button/ui/index.tsx
+++ src/shared/ui/Button/ui/index.tsx
@@ -13,13 +13,13 @@ export const Button: React.FC<PropsWithChildren<ButtonProps>> = ({
   return (
     <button
       className={cn(
-        "flex items-center justify-center rounded-2xl transition-all duration-300 ease-in-out",
+        "desktop:rounded-2xl flex items-center justify-center rounded-md transition-all duration-300 ease-in-out",
         appearance === "primary" &&
-          "w-491 h-82 bg-brown p text-white hover:opacity-80",
+          "desktop:w-491 desktop:h-82 bg-brown p w-238 h-41 text-white hover:opacity-80",
         appearance === "outline" &&
-          "border-brown w-491 h-82 p background-transparent border-2 text-black hover:opacity-80",
+          "border-brown desktop:w-491 desktop:h-82 p background-transparent w-238 h-41 border-2 text-black hover:opacity-80",
         appearance === "accent" &&
-          "w-234 h-66 bg-light-grey h3 hover:bg-brown text-black hover:text-white",
+          "desktop:w-234 desktop:h-66 bg-light-grey h3 hover:bg-brown w-156 h-33 text-black hover:text-white",
         disabled && "pointer-events-none opacity-30",
         className
       )}

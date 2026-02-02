5aa8c14c517bc2bd9cc5332f70aee7ba075e0c0f
spleereak <spleereak@gmail.com>
2026-02-02 10:39:29 +0300

    chore: поменял цвет бг у изображения карточки товара

--- src/pages/home/components/ProductCard/ui/index.tsx
+++ src/pages/home/components/ProductCard/ui/index.tsx
@@ -53,7 +53,7 @@ export const ProductCard: React.FC<ProductCardProps> = ({
           className="desktop:h-490 h-168 desktop:rounded-t-2xl w-full rounded-t-md object-cover"
         />
       ) : (
-        <div className="bg-light-grey desktop:size-490 size-168 desktop:rounded-t-2xl rounded-t-md" />
+        <div className="desktop:size-490 size-168 desktop:rounded-t-2xl rounded-t-md bg-[#D9D9D9]" />
       )}
       <div className="desktop:p-24 desktop:gap-38 desktop:rounded-b-2xl flex flex-col gap-12 rounded-b-md bg-white p-6">
         <p className="text_p--switch line-clamp-2">{title}</p>

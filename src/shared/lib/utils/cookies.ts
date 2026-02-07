bb5a1c15c4c6967e4c16665560c6de5e198b83f9
spleereak <spleereak@gmail.com>
2026-02-08 01:39:02 +0300

    fix: фикс SSR'а

--- src/shared/lib/utils/cookies.ts
+++ src/shared/lib/utils/cookies.ts
@@ -1,5 +1,8 @@
+const isBrowser = typeof document !== "undefined";
+
 export const cookies = {
   set: (name: string, value: string): void => {
+    if (!isBrowser) return;
     const date = new Date();
     date.setTime(date.getTime() + 24 * 60 * 60 * 1000 * 7);
     const expires = `expires=${date.toUTCString()}`;
@@ -7,11 +10,13 @@ export const cookies = {
   },
 
   get: (name: string): string | null => {
+    if (!isBrowser) return null;
     const cookies = document.cookie.split(";");
     const cookie = cookies.find(c => c.trim().startsWith(`${name}=`));
     return cookie ? cookie.split("=")[1] : null;
   },
   remove: (name: string): void => {
+    if (!isBrowser) return;
     document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;SameSite=None;Secure`;
   },
 

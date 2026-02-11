dab5c2aadf1ef73bdd3a45406ca647a35eb532d2
spleereak <spleereak@gmail.com>
2026-02-11 21:59:45 +0300

    fix: поправил иконку

--- src/shared/icons/ArrowDown.tsx
+++ src/shared/icons/ArrowDown.tsx
@@ -5,7 +5,7 @@ import { cn } from "../lib/utils/cn";
 export const ArrowDown = ({ className }: { className?: string }) => {
   return (
     <Image
-      src="http://localhost:3000/icons/arrow-down.png"
+      src="/icons/arrow-down.png"
       alt=""
       width={10}
       height={16}
